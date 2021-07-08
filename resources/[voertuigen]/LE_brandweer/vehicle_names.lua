b0eb34f5070dbc17c5c6a4575fca98ac98e31c17
BertLeidenRP <85788769+BertLeidenRP@users.noreply.github.com>
2021-07-08 20:36:25 +0200

    KAAS

--- resources/[voertuigen]/LE_brandweer/vehicle_names.lua
+++
@@ -1,2 +0,0 @@
-AddTextEntryByHash(GetHashKey("br1"),   "Brandweer Atego")                      --180 km/h
-AddTextEntryByHash(GetHashKey("br2"),   "Brandweer Volvo FM 320 HV")             --180 km/h
