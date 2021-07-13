7791e253811301d054622814d21550d61def322a
BertLeidenRP <85788769+BertLeidenRP@users.noreply.github.com>
2021-07-13 14:36:06 +0200

    bank ov + cardealer

--- resources/[esx]/esx_vehicleshop/config.lua
+++ resources/[esx]/esx_vehicleshop/config.lua
@@ -1,7 +1,7 @@
 Config                            = {}
 Config.DrawDistance               = 100
 Config.MarkerColor                = {r = 120, g = 120, b = 240}
-Config.EnablePlayerManagement     = false -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
+Config.EnablePlayerManagement     = true -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
 Config.ResellPercentage           = 50
 
 Config.Locale                     = 'en'
@@ -17,33 +17,33 @@ Config.PlateUseSpace = true
 Config.Zones = {
 
 	ShopEntering = {
-		Pos   = vector3(-796.33, -220.51, 36.08),
+		Pos   = vector3(-1176.23, -1705.81, 3.49),
 		Size  = {x = 1.5, y = 1.5, z = 1.0},
 		Type  = 1
 	},
 
 	ShopInside = {
-		Pos     = vector3(-783.98, -224.19, 36.32),
+		Pos     = vector3(1207.32, -1735.44, 4.49), 
 		Size    = {x = 1.5, y = 1.5, z = 1.0},
 		Heading = -20.0,
 		Type    = -1
 	},
 
 	ShopOutside = {
-		Pos     = vector3(-771.75, -231.96, 36.08),
+		Pos     = vector3(-1205.25, -1745.56, 4.45),
 		Size    = {x = 1.5, y = 1.5, z = 1.0},
 		Heading = 330.0,
 		Type    = -1
 	},
 
 	BossActions = {
-		Pos   = vector3(-32.0, -1114.2, 25.4),
+		Pos   = vector3(-1180.07, -1700.27, 3.45),
 		Size  = {x = 1.5, y = 1.5, z = 1.0},
 		Type  = -1
 	},
 
 	GiveBackVehicle = {
-		Pos   = vector3(-18.2, -1078.5, 25.6),
+		Pos   = vector3(-1207.27, -1744.61, 3.45),
 		Size  = {x = 3.0, y = 3.0, z = 1.0},
 		Type  = (Config.EnablePlayerManagement and 1 or -1)
 	},
