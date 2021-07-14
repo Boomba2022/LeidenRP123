9d5c780951fe9e2d0c7f5cb50dcdb3669ea55c1b
BertLeidenRP <85788769+BertLeidenRP@users.noreply.github.com>
2021-07-14 11:50:28 +0200

    garbage

---
+++ resources/[esx]/garbage/server.lua
@@ -0,0 +1,107 @@
+ESX = nil
+
+local dumpsterItems = {
+    [1] = {chance = 7, id = 'thermal_charge', name = 'Thermal Charge', quantity = math.random(1,3), limit = 10},
+    [2] = {chance = 4, id = 'h_laptop', name = 'Hacking Laptop', quantity = 1, limit = 4},
+    [3] = {chance = 4, id = 'id_card', name = 'ID Kaart', quantity = 1, limit = 10},
+    [4] = {chance = 3, id = 'lockpick', name = 'LockPick', quantity = 1, limit = 10},
+    [5] = {chance = 2, id = 'knife', name = 'mes', quantity = math.random(1,8), limit = 0},
+    [6] = {chance = 9, id = 'water', name = 'water', quantity = 1, limit = 2},
+    [7] = {chance = 5, id = 'fixkit', name = 'Reparatie Kit', quantity = math.random(1,2), limit = 0},
+    [8] = {chance = 7, id = 'coke', name = 'coke', quantity = 14, limit = 0},
+    [9] = {chance = 1, id = 'molotov', name = 'Molotov', quantity = 1, limit = 0},
+   }
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterUsableItem('wallet', function(source) --Hammer high time to unlock but 100% call cops
+    local source = tonumber(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local cash = math.random(20, 120)
+    local chance = math.random(1,2)
+
+    if chance == 2 then
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find $' .. cash .. ' inside the wallet'})
+        xPlayer.addMoney(cash)
+        local cardChance = math.random(1, 40)
+        if cardChance == 20 then
+            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find a Green Keycard inside the wallet'})
+            xPlayer.addInventoryItem('green-keycard', 1)
+        end
+    else
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'The wallet was empty'})
+    end
+
+    xPlayer.removeInventoryItem('wallet', 1)
+end)
+
+RegisterServerEvent('onyx:startDumpsterTimer')
+AddEventHandler('onyx:startDumpsterTimer', function(dumpster)
+    startTimer(source, dumpster)
+end)
+
+RegisterServerEvent('onyx:giveDumpsterReward')
+AddEventHandler('onyx:giveDumpsterReward', function()
+    local source = tonumber(source)
+    local item = {}
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local gotID = {}
+    local rolls = math.random(1, 2)
+    local foundItem = false
+
+    for i = 1, rolls do
+        item = dumpsterItems[math.random(1, #dumpsterItems)]
+        if math.random(1, 10) >= item.chance then
+            if item.isWeapon and not gotID[item.id] then
+                if item.limit > 0 then
+                    local count = xPlayer.getInventoryItem(item.id).count
+                    if count >= item.limit then
+                        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find a ' .. item.name .. ' but cannot carry any more of this item'})
+                    else
+                        gotID[item.id] = true
+                        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find a ' .. item.name})
+                        foundItem = true
+                        xPlayer.addWeapon(item.id, 50)
+                    end
+                else
+                    gotID[item.id] = true
+                    TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find a ' .. item.name})
+                    foundItem = true
+                    xPlayer.addWeapon(item.id, 50)
+                end
+            elseif not gotID[item.id] then
+                if item.limit > 0 then
+                    local count = xPlayer.getInventoryItem(item.id).count
+                    if count >= item.limit then
+                        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find ' .. item.quantity .. 'x ' .. item.name .. ' but cannot carry any more of this item'})
+                    else
+                        gotID[item.id] = true
+                        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find ' .. item.quantity .. 'x ' .. item.name})
+                        xPlayer.addInventoryItem(item.id, item.quantity)
+                        foundItem = true
+                    end
+                else
+                    gotID[item.id] = true
+                    TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find ' .. item.quantity .. 'x ' .. item.name})
+                    xPlayer.addInventoryItem(item.id, item.quantity)
+                    foundItem = true
+                end
+            end
+        end
+        if i == rolls and not gotID[item.id] and not foundItem then
+            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You find nothing'})
+        end
+    end
+end)
+
+function startTimer(id, object)
+    local timer = 10 * 60000
+
+    while timer > 0 do
+        Wait(1000)
+        timer = timer - 1000
+        if timer == 0 then
+            TriggerClientEvent('onyx:removeDumpster', id, object)
+        end
+    end
+end
